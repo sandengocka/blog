38db083de48d8002104693d6b774155a954486ea
sanden <sandguitar@gmail.com>
2025-03-30 23:54:00 -0700

    feat: rebranded to sanden

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,25 @@
-import { BlogPosts } from 'app/components/posts'
+import { BlogPosts } from "app/components/posts";
 
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">Sanden</h1>
       <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+        Building{" "}
+        <a
+          href="https://www.tryearmark.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:underline"
+        >
+          <strong>earmark</strong>
+        </a>{" "}
+        🚀 • Previously Director of Engineering @productplan, led iOS📱@mindbody
+        • Obsessed with great UX and sharing learnings along the way ❤️
       </p>
       <div className="my-8">
         <BlogPosts />
       </div>
     </section>
-  )
+  );
 }
